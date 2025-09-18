85e10772d3248930a65561ab9c9a2aa2a043b3eb
Magicabhi <abhipanchgalle001@gmail.com>
2025-09-18 20:50:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,76 @@
-$(document).ready(function () {
-  // Sticky navbar + scroll button
-  $(window).scroll(function () {
-    if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
-    }
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
-    } else {
-      $(".scroll-up-btn").removeClass("show");
-    }
-  });
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
-  // Scroll up
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-    $("html").css("scrollBehavior", "auto");
-  });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
-  // Smooth scroll on menu click
-  $(".navbar .menu li a").click(function () {
-    $("html").css("scrollBehavior", "smooth");
-  });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
-  // Toggle menu/navbar
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
-  });
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-  // Typing animation
-  var typed = new Typed(".typing", {
-    strings: [
-      "WordPress Developer",
-      "Full-Stack Developer",
-      "Frontend Engineer",
-      "Backend Developer",
-      "Freelancer"
-    ],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-  var typed2 = new Typed(".typing-2", {
-    strings: [
-      "WordPress Developer",
-      "Full-Stack Developer",
-      "Frontend Engineer",
-      "Backend Developer",
-      "Freelancer"
-    ],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
-
-  // Projects carousel
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: { items: 1, nav: false },
-      600: { items: 2, nav: false },
-      1000: { items: 3, nav: false }
-    }
-  });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
+
+/*  import google fonts */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Ubuntu:wght@400;500;700&display=swap');
